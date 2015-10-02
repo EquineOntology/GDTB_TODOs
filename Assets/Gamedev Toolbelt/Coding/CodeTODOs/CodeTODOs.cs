e7f7c1c0fdea362adf3c11c5951056279605876a
Christian Fratta <christian.fratta@gmail.com>
2015-10-02 15:41:47 +0200

    GUISkin assignment

--- Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOs.cs
+++ Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOs.cs
@@ -7,7 +7,7 @@ public class CodeTODOs : EditorWindow
 {
     // ====================================================================
     // ============== Variables used in more than one method ==============
-    // ====================================================================          
+    // ====================================================================
     //public ReorderableList _reorderableQQQs;
     public List<QQQ> _qqqs = new List<QQQ>();
     //private List<int> _priorities = new List<int>();
@@ -16,23 +16,24 @@ public class CodeTODOs : EditorWindow
 
     // ====================================================================
     // =========================== Editor stuff ===========================
-    // ==================================================================== 
+    // ====================================================================
     private const int BUTTON_WIDTH = 100;
     private const int EDITOR_WINDOW_MINSIZE_X = 300;
     private const int EDITOR_WINDOW_MINSIZE_Y = 300;
     private const string WINDOW_TITLE = "CodeTODOs";
     private GUISkin _GDTBSkin;
-     
+    private bool _GUISkinWasAssigned = false;
+
     // ====================================================================
     // ======================= Class functionality ========================
-    // ==================================================================== 
+    // ====================================================================
     [MenuItem("Gamedev Toolbelt/CodeTODOs")]
     public static void Init()
     {
         // Get existing open window or if none, make a new one.
         CodeTODOs window = (CodeTODOs)EditorWindow.GetWindow(typeof(CodeTODOs));
-        window.title = WINDOW_TITLE;
-        window.minSize = new Vector2(EDITOR_WINDOW_MINSIZE_X, EDITOR_WINDOW_MINSIZE_Y);    
+        window.titleContent = new GUIContent(WINDOW_TITLE);
+        window.minSize = new Vector2(EDITOR_WINDOW_MINSIZE_X, EDITOR_WINDOW_MINSIZE_Y);
         window.Show();
     }
 
@@ -43,14 +44,13 @@ public class CodeTODOs : EditorWindow
         {
             //var todoType = ScriptableObject.FindObjectOfType(typeof(CodeTodos))
             _GDTBSkin = GDTB_IOUtils.GetGUISkin();
-            GUI.skin = _GDTBSkin; 
+            _GUISkinWasAssigned = true;
         }
         else
         {
-            GUI.skin = _GDTBSkin;
-
+            _GUISkinWasAssigned = true;
         }
-        
+
         _showOptions = new AnimBool(false);
         _qqqs = CodeTODOsHelper.CheckAllScriptsForQQQs(out _qqqTasks, out _qqqScripts);
         /*_reorderableQQQs = new ReorderableList(_qqqs, typeof(QQQ), true, true, true, true);
@@ -70,6 +70,10 @@ public class CodeTODOs : EditorWindow
 
     private void OnGUI()
     {
+        if (_GUISkinWasAssigned == true)
+        {
+            GUI.skin = _GDTBSkin;
+        }
         EditorGUILayout.BeginVertical();
         DrawQQQList();
         EditorGUILayout.Space();
@@ -78,29 +82,29 @@ public class CodeTODOs : EditorWindow
         DrawOptions();
         EditorGUILayout.EndVertical();
     }
-    
-    private const int BOX_WIDTH = 400;    
+
+    private const int BOX_WIDTH = 400;
     private void DrawQQQList()
     {
         EditorGUILayout.BeginVertical();
-        
+
         for(int i= 0; i< _qqqs.Count; i++)
         {
-            EditorGUILayout.BeginHorizontal(EditorStyles.helpBox, GUILayout.Width(BOX_WIDTH));            
-            
+            EditorGUILayout.BeginHorizontal(EditorStyles.helpBox, GUILayout.Width(BOX_WIDTH));
+
             EditorGUILayout.BeginVertical();
-            
-            EditorGUILayout.BeginHorizontal();            
+
+            EditorGUILayout.BeginHorizontal();
             GUILayout.Space(10);
-            EditorGUILayout.LabelField(_qqqs[i].Task, EditorStyles.boldLabel, GUILayout.Width(BOX_WIDTH - 20));            
+            EditorGUILayout.LabelField(_qqqs[i].Task, EditorStyles.boldLabel, GUILayout.Width(BOX_WIDTH - 20));
             EditorGUILayout.EndHorizontal();
             EditorGUILayout.LabelField("In \"" + _qqqs[i].Script + "\".", GUILayout.Width(BOX_WIDTH - 20));
 
             EditorGUILayout.EndVertical();
-            
+
             EditorGUILayout.EndHorizontal();
         }
-        
+
         EditorGUILayout.EndVertical();
 
         //_reorderableQQQs.DoLayoutList();
@@ -110,7 +114,7 @@ public class CodeTODOs : EditorWindow
     private const string LIST_QQQS = "Refresh list";
     private void DrawListButton()
     {
-        EditorGUILayout.BeginHorizontal();    
+        EditorGUILayout.BeginHorizontal();
         EditorGUILayout.Space();
         if (GUILayout.Button(LIST_QQQS, GUILayout.Width(BUTTON_WIDTH)))
         {
@@ -123,10 +127,10 @@ public class CodeTODOs : EditorWindow
 
     // ===========================================================
     // =======================   OPTIONS   =======================
-    // ===========================================================    
-	
+    // ===========================================================
+
     // TODO token (QQQ)
-    public const string PREFS_CODETODOS_TOKEN = "GDTB_CodeTODOs_TODOToken";    
+    public const string PREFS_CODETODOS_TOKEN = "GDTB_CodeTODOs_TODOToken";
     public static string TODOToken = "QQQ";
     private string _oldTODOTokenValue = "QQQ";
 
@@ -139,19 +143,19 @@ public class CodeTODOs : EditorWindow
     public const string PREFS_CODETODOS_CHARS_BEFORE_NEWLINE = "GDTB_CodeTODOs_CharsBeforeNewline";
     private int _charsBeforeNewline = 60;
     private int _oldCharsBeforeNewlineValue = 60;
-    
+
     private AnimBool _showOptions;
-    
+
     private void DrawOptions()
     {
         EditorGUILayout.BeginVertical();
-        
+
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.Space();
         _showOptions.target = GUILayout.Toggle(_showOptions.value, "Show Options", "Button", GUILayout.Width(BUTTON_WIDTH));
         EditorGUILayout.Space();
         EditorGUILayout.EndHorizontal();
-        
+
         /*  ===================================================
          * | Options                                           |
          * |   TODO token ____________________                 |
@@ -162,7 +166,7 @@ public class CodeTODOs : EditorWindow
 		{
             EditorGUILayout.LabelField("Options", EditorStyles.boldLabel);
             EditorGUI.indentLevel++;
-			
+
 			// TODO token.
             TODOToken = EditorGUILayout.TextField("TODO token", TODOToken);
             if (TODOToken != _oldTODOTokenValue)
@@ -183,30 +187,30 @@ public class CodeTODOs : EditorWindow
             }
             EditorGUI.indentLevel--;
         }
-		EditorGUILayout.EndFadeGroup();        
+		EditorGUILayout.EndFadeGroup();
         EditorGUILayout.EndVertical();
     }
-    
+
     private void UpdateQQQTemplate(string newToken)
     {
         EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, newToken);
         _oldTODOTokenValue = newToken;
     }
-    
+
     private void UpdateCharsBeforeNewline(int newCharLimit)
     {
         EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, newCharLimit);
         _oldCharsBeforeNewlineValue = newCharLimit;
     }
-    
+
     private void UpdateCutoffSwitch(bool newSwitchValue)
     {
         EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, newSwitchValue);
         _oldCutoffSwitchValue = newSwitchValue;
     }
-    
+
     private void UpdateEditorPrefs()
-    {        
+    {
         /// TODO token.
         if(!EditorPrefs.HasKey(PREFS_CODETODOS_TOKEN))
         {
@@ -216,7 +220,7 @@ public class CodeTODOs : EditorWindow
         {
             TODOToken = EditorPrefs.GetString(PREFS_CODETODOS_TOKEN, "QQQ");
         }
-        
+
         // Chars before newline.
         if(!EditorPrefs.HasKey(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE))
         {
@@ -226,7 +230,7 @@ public class CodeTODOs : EditorWindow
         {
             _charsBeforeNewline = EditorPrefs.GetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, 60);
         }
-        
+
         // Cutoff or newline.
         if(!EditorPrefs.HasKey(PREFS_CODETODOS_CUTOFF))
         {
---
+++ Assets/Gamedev Toolbelt/Coding/GDTB_IOUtils.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public static class GDTB_IOUtils {
+
+	public static GUISkin GetGUISkin()
+	{
+        GUISkin skin = AssetDatabase.LoadAssetAtPath(GetFirstInstanceOfFile("GDTBSkin.guiskin"), typeof(GUISkin)) as GUISkin;
+        return skin;
+    }
+
+	private static string GetFirstInstanceOfFile(string fileName)
+	{
+        var projectDirectoryPath = Directory.GetCurrentDirectory();
+        var projectDirectoryInfo = new DirectoryInfo(projectDirectoryPath);
+        var listOfAssetsDirs = projectDirectoryInfo.GetDirectories("Assets");
+        var assetsDir = "";
+        foreach(var dir in listOfAssetsDirs)
+        {
+            if (dir.FullName.EndsWith("\\Assets"))
+            {
+                assetsDir = dir.FullName;
+            }
+        }
+        var path = assetsDir;
+
+        var q = new Queue<string>();
+        q.Enqueue(path);
+        var absolutePath = "";
+        while (q.Count > 0)
+        {
+            path = q.Dequeue();
+            try
+            {
+                foreach (string subDir in Directory.GetDirectories(path))
+                {
+                    q.Enqueue(subDir);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+            string[] files = null;
+            try
+            {
+                files = Directory.GetFiles(path);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError(ex);
+            }
+            if (files != null)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if (files[i].EndsWith(fileName))
+                    {
+                        absolutePath = files[i];
+                    }
+                }
+            }
+        }
+        var relativePath = absolutePath.Remove(0, projectDirectoryPath.Length + 1);
+        Debug.Log(relativePath);
+        return relativePath;
+    }
+}
