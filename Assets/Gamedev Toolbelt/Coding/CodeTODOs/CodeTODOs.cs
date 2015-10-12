9ced934221763e9831eea8f7b81b612e61c33699
Christian Fratta <christian.fratta@gmail.com>
2015-10-12 23:49:59 +0200

    Moved settings to Preferences window

--- Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOs.cs
+++ Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOs.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityEditor.AnimatedValues;
 using System.Collections.Generic;
 
 public class CodeTODOs : EditorWindow
@@ -40,10 +39,9 @@ public class CodeTODOs : EditorWindow
 
     public void OnEnable()
     {
-        UpdateEditorPrefs();
+        //UpdateEditorPrefs();
         if(_GDTBSkin == null)
         {
-            //var todoType = ScriptableObject.FindObjectOfType(typeof(CodeTodos))
             _GDTBSkin = GDTB_IOUtils.GetGUISkin();
             _GUISkinWasAssigned = true;
         }
@@ -52,7 +50,6 @@ public class CodeTODOs : EditorWindow
             _GUISkinWasAssigned = true;
         }
 
-        _showOptions = new AnimBool(false);
         _qqqs = CodeTODOsHelper.CheckAllScriptsForQQQs(out _qqqTasks, out _qqqScripts);
         /*_reorderableQQQs = new ReorderableList(_qqqs, typeof(QQQ), true, true, true, true);
 
@@ -80,8 +77,6 @@ public class CodeTODOs : EditorWindow
         DrawQQQList();
         EditorGUILayout.Space();
         DrawListButton();
-        EditorGUILayout.Space();
-        DrawOptions();
         EditorGUILayout.EndVertical();
     }
 
@@ -94,9 +89,9 @@ public class CodeTODOs : EditorWindow
         for (int i = 0; i < _qqqs.Count; i++)
         {
             // Calculate how high the box must be to accomodate the task & script.
-            var taskLines = ((_qqqs[i].Task.Length / _charsBeforeNewline) > 0) && _cutoffSwitch == false ? (_qqqs[i].Task.Length / _charsBeforeNewline) + 1 : 1;
+            var taskLines = ((_qqqs[i].Task.Length / CodeTODOsPrefs.CharsBeforeNewline) > 0) && CodeTODOsPrefs.CutoffSwitch == false ? (_qqqs[i].Task.Length / CodeTODOsPrefs.CharsBeforeNewline) + 1 : 1;
             var taskHeight = taskLines * _characterHeightCoefficient * 1.1f;
-            var scriptLines = ((_qqqs[i].Script.Length / _charsBeforeNewline) > 0) && _cutoffSwitch == false ? (_qqqs[i].Script.Length / _charsBeforeNewline) + 1 : 1;
+            var scriptLines = ((_qqqs[i].Script.Length / CodeTODOsPrefs.CharsBeforeNewline) > 0) && CodeTODOsPrefs.CutoffSwitch == false ? (_qqqs[i].Script.Length / CodeTODOsPrefs.CharsBeforeNewline) + 1 : 1;
             var scriptHeight = scriptLines * _characterHeightCoefficient;
             var boxHeight = taskHeight + scriptHeight;
 
@@ -109,26 +104,26 @@ public class CodeTODOs : EditorWindow
             // Build correctly formatted "task" label.
             // The number of characters before a newline is reduced to account for the bold label style.
             string taskLabel;
-            if (_cutoffSwitch == true)
+            if (CodeTODOsPrefs.CutoffSwitch == true)
             {
-                taskLabel = CodeTODOsHelper.GetStringEnd(_qqqs[i].Task, _charsBeforeNewline - 8);
+                taskLabel = CodeTODOsHelper.GetStringEnd(_qqqs[i].Task, CodeTODOsPrefs.CharsBeforeNewline - 8);
             }
             else
             {
-                taskLabel = CodeTODOsHelper.DivideStringWithNewlines(_qqqs[i].Task, _charsBeforeNewline - 8);
+                taskLabel = CodeTODOsHelper.DivideStringWithNewlines(_qqqs[i].Task, CodeTODOsPrefs.CharsBeforeNewline - 8);
             }
             EditorGUILayout.LabelField(taskLabel, EditorStyles.boldLabel, GUILayout.Height(taskHeight));
             EditorGUILayout.EndHorizontal();
 
             // Build correctly formatted "script" label.
             string scriptLabel;
-            if (_cutoffSwitch == true)
+            if (CodeTODOsPrefs.CutoffSwitch == true)
             {
-                scriptLabel = "In \"..." + CodeTODOsHelper.GetStringEnd(_qqqs[i].Script, _charsBeforeNewline) + "\"";
+                scriptLabel = "In \"..." + CodeTODOsHelper.GetStringEnd(_qqqs[i].Script, CodeTODOsPrefs.CharsBeforeNewline) + "\"";
             }
             else
             {
-                scriptLabel = "In \"" + CodeTODOsHelper.DivideStringWithNewlines(_qqqs[i].Script, _charsBeforeNewline) + "\"";
+                scriptLabel = "In \"" + CodeTODOsHelper.DivideStringWithNewlines(_qqqs[i].Script, CodeTODOsPrefs.CharsBeforeNewline) + "\"";
             }
             EditorGUILayout.LabelField(scriptLabel, GUILayout.Height(scriptHeight));
 
@@ -154,122 +149,5 @@ public class CodeTODOs : EditorWindow
         }
         EditorGUILayout.Space();
         EditorGUILayout.EndHorizontal();
-    }
-
-    // ===========================================================
-    // =======================   OPTIONS   =======================
-    // ===========================================================
-
-    // TODO token (QQQ)
-    public const string PREFS_CODETODOS_TOKEN = "GDTB_CodeTODOs_TODOToken";
-    public static string TODOToken = "QQQ";
-    private string _oldTODOTokenValue = "QQQ";
-
-    // Cutoff or newline
-    public const string PREFS_CODETODOS_CUTOFF = "GDTB_CodeTODOs_Cutoff";
-    private bool _cutoffSwitch = true;
-    private bool _oldCutoffSwitchValue = true;
-
-    // Characters in QQQs before newline/cutoff
-    public const string PREFS_CODETODOS_CHARS_BEFORE_NEWLINE = "GDTB_CodeTODOs_CharsBeforeNewline";
-    private int _charsBeforeNewline = 40;
-    private int _oldCharsBeforeNewlineValue = 50;
-
-    private AnimBool _showOptions;
-
-    private void DrawOptions()
-    {
-        EditorGUILayout.BeginVertical();
-
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.Space();
-        _showOptions.target = GUILayout.Toggle(_showOptions.value, "Show Options", "Button", GUILayout.Width(BUTTON_WIDTH));
-        EditorGUILayout.Space();
-        EditorGUILayout.EndHorizontal();
-
-        /*  ===================================================
-         * | Options                                           |
-         * |   TODO token ____________________                 |
-         * |   Cutoff TODOs _                                  |
-         * |   Characters before newline __                    |
-         *  =================================================== */
-        if (EditorGUILayout.BeginFadeGroup(_showOptions.faded))
-		{
-            EditorGUILayout.LabelField("Options", EditorStyles.boldLabel);
-            EditorGUI.indentLevel++;
-
-			// TODO token.
-            TODOToken = EditorGUILayout.TextField("TODO token", TODOToken);
-            if (TODOToken != _oldTODOTokenValue)
-            {
-                UpdateQQQTemplate(TODOToken);
-            }
-            // Cutoff TODOs
-            _cutoffSwitch = EditorGUILayout.Toggle("Cutoff TODOs", _cutoffSwitch);
-            if(_cutoffSwitch != _oldCutoffSwitchValue)
-            {
-                UpdateCutoffSwitch(_cutoffSwitch);
-            }
-            // Chars before newline.
-            _charsBeforeNewline = EditorGUILayout.IntField("Characters on line", _charsBeforeNewline);
-            if (_charsBeforeNewline != _oldCharsBeforeNewlineValue)
-            {
-                UpdateCharsBeforeNewline(_charsBeforeNewline);
-            }
-            EditorGUI.indentLevel--;
-        }
-		EditorGUILayout.EndFadeGroup();
-        EditorGUILayout.EndVertical();
-    }
-
-    private void UpdateQQQTemplate(string newToken)
-    {
-        EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, newToken);
-        _oldTODOTokenValue = newToken;
-    }
-
-    private void UpdateCharsBeforeNewline(int newCharLimit)
-    {
-        EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, newCharLimit);
-        _oldCharsBeforeNewlineValue = newCharLimit;
-    }
-
-    private void UpdateCutoffSwitch(bool newSwitchValue)
-    {
-        EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, newSwitchValue);
-        _oldCutoffSwitchValue = newSwitchValue;
-    }
-
-    private void UpdateEditorPrefs()
-    {
-        /// TODO token.
-        if(!EditorPrefs.HasKey(PREFS_CODETODOS_TOKEN))
-        {
-            EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, "QQQ");
-        }
-        else
-        {
-            TODOToken = EditorPrefs.GetString(PREFS_CODETODOS_TOKEN, "QQQ");
-        }
-
-        // Chars before newline.
-        if(!EditorPrefs.HasKey(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE))
-        {
-            EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, 60);
-        }
-        else
-        {
-            _charsBeforeNewline = EditorPrefs.GetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, 60);
-        }
-
-        // Cutoff or newline.
-        if(!EditorPrefs.HasKey(PREFS_CODETODOS_CUTOFF))
-        {
-            EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, false);
-        }
-        else
-        {
-            _cutoffSwitch = EditorPrefs.GetBool(PREFS_CODETODOS_CUTOFF, false);
-        }
     }
 }
--- Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOsPrefs.cs
+++ Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOsPrefs.cs
@@ -1,15 +1,104 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEditor;
 
-public class CodeTODOsPrefs : MonoBehaviour {
+public class CodeTODOsPrefs
+{
+  // TODO token (QQQ)
+    public const string PREFS_CODETODOS_TOKEN = "GDTB_CodeTODOs_TODOToken";
+    private static string _todoToken = "QQQ";
+    public static string TODOToken
+    {
+        get { return _todoToken; }
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Cutoff or newline
+    public const string PREFS_CODETODOS_CUTOFF = "GDTB_CodeTODOs_Cutoff";
+    private static bool _cutoffSwitch = true;
+    public static bool CutoffSwitch
+    {
+        get { return _cutoffSwitch; }
+    }
+
+    // Characters in QQQs before newline/cutoff
+    public const string PREFS_CODETODOS_CHARS_BEFORE_NEWLINE = "GDTB_CodeTODOs_CharsBeforeNewline";
+    private static int _charsBeforeNewline = 40;
+    public static int CharsBeforeNewline
+    {
+        get { return _charsBeforeNewline; }
+    }
+
+    [PreferenceItem("Code TODOs")]
+    public static void PreferencesGUI()
+    {
+        /*  ===================================================
+         * | Options                                           |
+         * |   TODO token ____________________                 |
+         * |   Cutoff TODOs _                                  |
+         * |   Characters before newline __                    |
+         *  =================================================== */
+        EditorGUILayout.BeginVertical();
+        _todoToken = EditorGUILayout.TextField("TODO token", _todoToken);
+        _cutoffSwitch = EditorGUILayout.Toggle("Cutoff TODOs", _cutoffSwitch);
+        _charsBeforeNewline = EditorGUILayout.IntField("Characters on line", _charsBeforeNewline);
+
+        if(GUI.changed)
+        {
+            UpdatePreferences();
+        }
+    }
+
+    private static void UpdatePreferences()
+    {
+        UpdateQQQTemplate(_todoToken);
+        UpdateCutoffSwitch(_cutoffSwitch);
+        UpdateCharsBeforeNewline(_charsBeforeNewline);
+    }
+
+    private static void UpdateQQQTemplate(string newToken)
+    {
+        EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, newToken);
+    }
+
+    private static void UpdateCharsBeforeNewline(int newCharLimit)
+    {
+        EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, newCharLimit);
+    }
+
+    private static void UpdateCutoffSwitch(bool newSwitchValue)
+    {
+        EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, newSwitchValue);
+    }
+
+    private void UpdateEditorPrefs()
+    {
+        /// TODO token.
+        if (!EditorPrefs.HasKey(PREFS_CODETODOS_TOKEN))
+        {
+            EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, "QQQ");
+        }
+        else
+        {
+            _todoToken = EditorPrefs.GetString(PREFS_CODETODOS_TOKEN, "QQQ");
+        }
+
+        // Chars before newline.
+        if (!EditorPrefs.HasKey(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE))
+        {
+            EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, 60);
+        }
+        else
+        {
+            _charsBeforeNewline = EditorPrefs.GetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, 60);
+        }
+
+        // Cutoff or newline.
+        if (!EditorPrefs.HasKey(PREFS_CODETODOS_CUTOFF))
+        {
+            EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, false);
+        }
+        else
+        {
+            _cutoffSwitch = EditorPrefs.GetBool(PREFS_CODETODOS_CUTOFF, false);
+        }
+    }
 }
