48575f428b52d63bf92ec995c1370db9a9e5e9b5
Christian Fratta <christian.fratta@gmail.com>
2015-10-20 21:38:37 +0200

    Fixed typo in constants

--- Assets/Gamedev Toolbelt/GUI/GUIConstants.cs
+++ Assets/Gamedev Toolbelt/GUI/GUIConstants.cs
@@ -3,10 +3,9 @@
     public const string ICON_QQQ_URGENT = "/GUI/qqqPriority_URGENT.jpg";
     public const string ICON_QQQ_NORMAL = "/GUI/qqqPriority_NORMAL.jpg";
     public const string ICON_QQQ_MINOR = "/GUI/qqqPriority_MINOR.jpg";
-    public const string ICON_QQQ_DONE = "/GUI/qqq_DONE.psd";
+    public const string ICON_QQQ_DONE = "/GUI/qqq_DONE.jpg";
 
     public const string TEXT_QQQ_DONE = "Complete\ntask";
     public const string TEXT_REFRESH_LIST = "Force list refresh";
     public const string TEXT_WINDOW_TITLE = "CodeTODOs";
-
 }
