987dc76a604102a0f9db94c3c0c9bc427629ed2a
Christian Fratta <christian.fratta@gmail.com>
2015-12-16 18:35:54 +0100

    Remove unused preferences

--- Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOsPrefs.cs
+++ Assets/Gamedev Toolbelt/Coding/CodeTODOs/CodeTODOsPrefs.cs
@@ -14,24 +14,6 @@ public class CodeTODOsPrefs
     }
 
 
-    // Cutoff or newline
-    public const string PREFS_CODETODOS_CUTOFF = "GDTB_CodeTODOs_Cutoff";
-    private static bool _cutoffSwitch = true;
-    public static bool CutoffSwitch
-    {
-        get { return _cutoffSwitch; }
-    }
-
-
-    // Characters in QQQs before newline/cutoff
-    public const string PREFS_CODETODOS_CHARS_BEFORE_NEWLINE = "GDTB_CodeTODOs_CharsBeforeNewline";
-    private static int _charsBeforeNewline = 40;
-    public static int CharsBeforeNewline
-    {
-        get { return _charsBeforeNewline; }
-    }
-
-
     // Priority displayed as text, icon, text + icon
     public const string PREFS_CODETODOS_PRIORITY_DISPLAY = "GDTB_CodeTODOs_PriorityDisplay";
     private static PriorityDisplayFormat _priorityDisplay = PriorityDisplayFormat.ICON_ONLY;
@@ -41,21 +23,14 @@ public class CodeTODOsPrefs
     }
     private static string[] _displayFormatsString = { "Text only", "Icon only", "Icon and Text" };
 
+
     [PreferenceItem("Code TODOs")]
     public static void PreferencesGUI()
     {
-        /*  ===============================================
-         * | Options                                       |
-         * |   TODO token ____________________             |
-         * |   Cutoff TODOs __________________             |
-         * |   //Characters before newline _____             |
-         * |   Priority format _______________             |
-         *  =============================================== */
         UpdateEditorPrefs();
         EditorGUILayout.BeginVertical();
         _todoToken = EditorGUILayout.TextField("TODO token", _todoToken);
-        //_cutoffSwitch = EditorGUILayout.Toggle("Cutoff TODOs", _cutoffSwitch);
-        //_charsBeforeNewline = EditorGUILayout.IntField("Characters on line", _charsBeforeNewline);
+
         _priorityDisplay = (PriorityDisplayFormat) EditorGUILayout.Popup("Priority format", System.Convert.ToInt16(_priorityDisplay), _displayFormatsString);
 
         if (GUI.changed)
@@ -68,8 +43,6 @@ public class CodeTODOsPrefs
     private static void UpdatePreferences()
     {
         UpdateQQQTemplate(_todoToken);
-        //UpdateCutoffSwitch(_cutoffSwitch);
-        //UpdateCharsBeforeNewline(_charsBeforeNewline);
         UpdatePriorityDisplay(_priorityDisplay);
     }
 
@@ -78,20 +51,6 @@ public class CodeTODOsPrefs
     {
         EditorPrefs.SetString(PREFS_CODETODOS_TOKEN, aNewToken);
         _todoToken = "QQQ";
-    }
-
-
-    private static void UpdateCharsBeforeNewline(int aNewCharLimit)
-    {
-        EditorPrefs.SetInt(PREFS_CODETODOS_CHARS_BEFORE_NEWLINE, aNewCharLimit);
-        _charsBeforeNewline = 60;
-    }
-
-
-    private static void UpdateCutoffSwitch(bool aNewSwitchValue)
-    {
-        EditorPrefs.SetBool(PREFS_CODETODOS_CUTOFF, aNewSwitchValue);
-        _cutoffSwitch = false;
     }
 
 
