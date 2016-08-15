bc5d68e2b177ac04fed8fd80ee7b32d09f05d6e6
Christian Fratta <christian.fratta@gmail.com>
2016-08-15 09:16:59 +0200

    Add ifdef to QQQOps to make codeTODOs compatible with 4.5 >

--- Assets/Gamedev Toolbelt/Editor/CodeTODOs/QQQOps.cs
+++ Assets/Gamedev Toolbelt/Editor/CodeTODOs/QQQOps.cs
@@ -249,8 +249,19 @@ namespace com.immortalhydra.gdtb.codetodos
         /// Open the script associated with the qqq in question.
         public static void OpenScript(QQQ aQQQ)
         {
+            #if UNITY_4
+            var script = AssetDatabase.LoadAssetAtPath<UnityEngine.TextAsset>(aQQQ.Script, typeof(UnityEngine.TextAsset)) as UnityEngine.TextAsset;
+            AssetDatabase.OpenAsset(script.GetInstanceID(), (aQQQ.LineNumber + 1));
+
+            #elif UNITY_5_3_OR_NEWER
             var script = AssetDatabase.LoadAssetAtPath<UnityEngine.TextAsset>(aQQQ.Script) as UnityEngine.TextAsset;
             AssetDatabase.OpenAsset(script.GetInstanceID(), (aQQQ.LineNumber + 1));
+
+            #elif UNITY_5
+            var script = AssetDatabase.LoadAssetAtPath<UnityEngine.TextAsset>(aQQQ.Script, typeof(UnityEngine.TextAsset)) as UnityEngine.TextAsset;
+            AssetDatabase.OpenAsset(script.GetInstanceID(), (aQQQ.LineNumber + 1));
+
+            #endif
         }
 
 
